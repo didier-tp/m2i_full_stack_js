6dfad0101fe8efa12fa4fc241766cda8b9bf9924
didier <didier@d-defrance.fr>
2023-04-27 15:12:35 +0200

    s

--- my-app/src/app/common/service/preferences.service.ts
+++ my-app/src/app/common/service/preferences.service.ts
@@ -5,7 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class PreferencesService {
 
-  public couleurFondPreferee :string = 'lightgrey'; //v1 : public
+  //public couleurFondPreferee :string = 'lightgrey'; //v1 : public
 
-  constructor() { }
+  private _couleurFondPreferee: string = "?"; //v2 : private + get/set + localStorage
+  public get couleurFondPreferee() {
+    return this._couleurFondPreferee;
+  }
+  public set couleurFondPreferee(c: string) {
+    this._couleurFondPreferee = c;
+    localStorage.setItem('preferences.couleurFond', c);
+  }
+  constructor() {
+    const c = localStorage.getItem('preferences.couleurFond');
+    this._couleurFondPreferee = c ? c : 'lightgrey';
+  }
 }
+
+
